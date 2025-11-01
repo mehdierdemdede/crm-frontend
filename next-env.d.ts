7a03c4588ae04fff020d9decd6879d6a60f7b039
Mehdi Erdem DEDE <mehdierdemdede@gmail.com>
2025-11-02 00:17:05 +0300

    Fix ESLint triple-slash reference warning

---
+++ next-env.d.ts
@@ -0,0 +1,8 @@
+import "next";
+import "next/image-types/global";
+import "./.next/types/routes.d.ts";
+
+// NOTE: This file is based on the default Next.js template.
+// The triple-slash directives have been replaced with import statements
+// to satisfy ESLint's `@typescript-eslint/triple-slash-reference` rule.
+// See https://nextjs.org/docs/app/api-reference/config/typescript for more information.
