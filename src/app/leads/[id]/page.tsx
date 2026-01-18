9cf6167af4bc952ac8d817df546db1d5323ab54d
Mehdi Erdem DEDE <mehdierdemdede@gmail.com>
2026-01-18 12:12:50 +0300

    edit reklam field

--- src/app/leads/[id]/page.tsx
+++ src/app/leads/[id]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useId, useState } from "react";
 
 import { ArrowLeft, Check, Copy, Facebook, MessageCircle, Pencil, Phone, X } from "lucide-react";
+import { useEffect, useId, useState } from "react";
 
 import { Button } from "@/components/Button";
 import { Card, CardContent, CardHeader } from "@/components/Card";
